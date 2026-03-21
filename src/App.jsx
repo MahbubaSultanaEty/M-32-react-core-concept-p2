79af823a5afcb60959ba24fca998794f6309037f
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-03-21 16:18:56 +0600

    event handling in many ways

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,44 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function handleClick() {
+  alert('I am clicked')
+}
+
+  const handleClick3 = () => {
+    alert('(arrow function) clicked me 3')
+  }
+
+  const handleMinus5 = (num) => {
+    const newNum = num - 5;
+    alert(`function wrapping (arrow function দিয়ে wrap করা), ${newNum}`)
+  }
+  
   return (
     <>
       <section id="center">
         
         <div>
           <h1>Get started</h1>
+          <button onClick={handleClick}>Click Me</button>
+          <button onClick={function handleClick2() {
+            alert('(inline function)CLicked Me 2')
+          }}>Click Me 2</button>
+
+          <button onClick={handleClick3}>Click ME 3</button>
+         
+          <button onClick={() => {
+            alert('(Inline arrow function )clicked Me 4')
+          }}>Click ME 4</button>
+
+          <button onClick={() => handleMinus5(15)}>CLick ME 5</button>
         </div>
         
+
+
       </section>
 
-     
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+    
     </>
   )
 }

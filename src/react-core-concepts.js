9433e0d800ffa2c4504cd5309a0913778b7da507
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-03-23 20:08:06 +0600

    react core concepts

---
+++ src/react-core-concepts.js
@@ -0,0 +1,13 @@
+/**
+ * 1. Components: components are like bread  and butter of your react. They are individual, reusable bits of code that serves as the bulding blocks of your ui. They are javascript functions that return HTML.
+ * 
+ * 2. JSX (JAvascipt XMl): JSX is a syntax extension that allows you to write html code directly inside your javasript . 
+ * 
+ * 3. Props: Props are how you pass the data from one component to another (especialy from parent to child). They are read only, meaning a compononent should never modify the props it recievs.
+ * 
+ * 4. State: WHile props are external and controlled by the parent , state is internal and controlled by the component itself. STate is used in data that chnges over time like in an input field or a counter).
+ * 
+ * 5. Hooks: Hooks are special functions that lets you hook into the react functional features. The two most common are:
+ * useState: allows you to add state to functional components.
+ * useEffect: allows you to perform side affects (like fetching data or manually changing the DOM ) after the component renders
+ */
